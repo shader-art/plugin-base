080ea6a794550d0ce8f72b73510ee68eecefd92f
Lea Rosema <terabaud@gmail.com>
2021-04-18 14:10:03 +0200

    feat: add onBeforeCompileShader

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,14 @@ export interface ShaderArtPlugin {
     program: WebGLProgram,
     canvas: HTMLCanvasElement
   ): void | Promise<void>;
+
+  onBeforeCompileShader?: (
+    hostElement: HTMLElement,
+    gl: WebGLRenderingContext | WebGL2RenderingContext,
+    program: WebGLProgram,
+    canvas: HTMLCanvasElement,
+    vertexShader: string,
+    fragmentShader: string
+  ) => void;
   dispose(): void;
 }
